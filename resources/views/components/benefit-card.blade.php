02d5bf8ab9f283f65f0985f1e0f765ec5a91ccff
Maarten van Willigen <maarten@websitebezorgd.nl>
2025-12-02 16:38:54 +0100

    benfit size text

--- resources/views/components/benefit-card.blade.php
+++ resources/views/components/benefit-card.blade.php
@@ -20,12 +20,21 @@ $text = $card['text'] ?? '';
   default => 'inherit',
   };
   }
+
+  // Map text style to prose modifier
+  $proseSize = match($textStyle) {
+  'u-text-style-small' => 'prose-p:text-small prose-ul:text-small prose-ol:text-small prose-li:text-small',
+  'u-text-style-main' => 'prose-p:text-main prose-ul:text-main prose-ol:text-main prose-li:text-main',
+  'u-text-style-medium' => 'prose-p:text-medium prose-ul:text-medium prose-ol:text-medium prose-li:text-medium',
+  'u-text-style-large' => 'prose-p:text-large prose-ul:text-large prose-ol:text-large prose-li:text-large',
+  default => 'prose-p:text-small prose-ul:text-small prose-ol:text-small prose-li:text-small',
+  };
   @endphp
 
   <div class="card w-full p-u-5 {{ $classes }}" data-theme="{{ $cardTheme }}">
     <div class="card_content-top content-wrapper u-margin-trim">
       @if ($icon)
-      <div class="card-icon mb-u-5">
+      <div class="card-icon mb-u-5 text-">
         <img src="{{ $icon['url'] }}" alt="{{ $icon['alt'] ?: $heading }}" class="w-u-6 h-u-6 object-contain" loading="lazy">
       </div>
       @endif
@@ -35,7 +44,7 @@ $text = $card['text'] ?? '';
       @endif
 
       @if ($text)
-      <div class="card-text {{ $textStyle }} u-margin-bottom-text prose">{!! $text !!}</div>
+      <div class="card-text prose {{ $proseSize }}">{!! $text !!}</div>
       @endif
     </div>
   </div>
