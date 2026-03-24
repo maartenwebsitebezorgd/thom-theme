07632443b4e5944f89d9e97a44d442449ea758a0
Maarten van Willigen <maarten@websitebezorgd.nl>
2026-03-24 14:00:24 +0100

    remove search from 404

--- resources/views/404.blade.php
+++ resources/views/404.blade.php
@@ -4,13 +4,13 @@
 
 @php
 $recentPosts = get_posts([
-  'post_type'      => 'post',
-  'posts_per_page' => 3,
-  'orderby'        => 'date',
-  'order'          => 'DESC',
+'post_type' => 'post',
+'posts_per_page' => 3,
+'orderby' => 'date',
+'order' => 'DESC',
 ]);
 
-$homeUrl    = home_url('/');
+$homeUrl = home_url('/');
 $contactUrl = get_permalink(get_page_by_path('contact'));
 @endphp
 
@@ -44,16 +44,6 @@ $contactUrl = get_permalink(get_page_by_path('contact'));
   </div>
 </section>
 
-{{-- Search --}}
-<section class="u-section pt-section-small pb-section-small">
-  <div class="u-container max-w-container-main">
-    <div class="mx-auto" style="max-width: 36rem;">
-      <p class="u-text-style-tagline mb-u-5 text-center">{{ __('Zoeken', 'sage') }}</p>
-      {!! get_search_form(false) !!}
-    </div>
-  </div>
-</section>
-
 {{-- Recent posts --}}
 @if ($recentPosts)
 <section data-theme="light" class="u-section pt-section-main pb-section-main">
@@ -67,18 +57,18 @@ $contactUrl = get_permalink(get_page_by_path('contact'));
     <div class="grid grid-cols-1 md:grid-cols-3 gap-u-4">
       @foreach ($recentPosts as $post)
       @php
-        $thumbnail  = get_post_thumbnail_id($post->ID);
-        $image      = $thumbnail ? wp_get_attachment_image_src($thumbnail, 'large') : null;
-        $categories = get_the_category($post->ID);
-        $category   = !empty($categories) ? $categories[0]->name : null;
-        $article = [
-          'image'             => $image ? ['url' => $image[0], 'alt' => get_post_meta($thumbnail, '_wp_attachment_image_alt', true)] : null,
-          'category'          => $category,
-          'title'             => $post->post_title,
-          'excerpt'           => get_the_excerpt($post),
-          'link'              => ['url' => get_permalink($post->ID), 'title' => __('Lees meer', 'sage'), 'target' => '_self'],
-          'make_card_clickable' => true,
-        ];
+      $thumbnail = get_post_thumbnail_id($post->ID);
+      $image = $thumbnail ? wp_get_attachment_image_src($thumbnail, 'large') : null;
+      $categories = get_the_category($post->ID);
+      $category = !empty($categories) ? $categories[0]->name : null;
+      $article = [
+      'image' => $image ? ['url' => $image[0], 'alt' => get_post_meta($thumbnail, '_wp_attachment_image_alt', true)] : null,
+      'category' => $category,
+      'title' => $post->post_title,
+      'excerpt' => get_the_excerpt($post),
+      'link' => ['url' => get_permalink($post->ID), 'title' => __('Lees meer', 'sage'), 'target' => '_self'],
+      'make_card_clickable' => true,
+      ];
       @endphp
       <div>
         <x-article-simple :article="$article" image-aspect-ratio="aspect-[3/2]" section-theme="light" />
